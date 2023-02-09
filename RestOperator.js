0d3fb41cec7c584997f032429219896ada35ded2
Imran Ali <imranalishahani7@gmail.com>
2023-02-09 12:46:56 +0500

    console chnage

--- RestOperator.js
+++ RestOperator.js
@@ -9,4 +9,5 @@ function sum(name,age, email,...arg){
 }
 
 sum("Joel Bhai","27","ab@gmail.com", 3,5);
-sum("Imran Ali", "21", "abc@gmail.com" ,10,10); 
+sum("Imran Ali", "21", "abc@gmail.com" ,10,10); console.log();[D[D[c = ""[D[C;[D[D[D[CAli[C";
+console.log();[D[D[D[Cc[C[[Cc);sum("Ali","20", "ali@gmail.com", 20,30);
